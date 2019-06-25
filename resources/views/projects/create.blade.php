2db7013330d3c1240f0b08c128ef14cfc62ab2b5
Robbozinoz <Robbozinoz>
2019-06-25 13:37:47 +0800

    First commit to end of lesson 10 csrf security

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Create</title>
+</head>
+<body>
+    <h1>Create a Project</h1>
+
+    <form method="POST" action="/projects">
+        {{ csrf_field() }}
+        <div>
+            <input type="text" name="title" placeholder="Project title">
+        </div>
+
+        <div>
+            <textarea name="description" placeholder="Project Description" cols="30" rows="10"></textarea>
+        </div>
+
+        <div>
+            <button type="submit">Create project</button>
+        </div>
+    </form>
+</body>
+</html>
